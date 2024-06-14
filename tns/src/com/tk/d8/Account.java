429e6426336e45459955cc7a9fc026ac52eb7720
Taheer Khan <Taheer Khan@DESKTOP-CLASFLV>
2024-06-14 20:47:18 +0530

    tns

---
+++ tns/src/com/tk/d8/Account.java
@@ -0,0 +1,67 @@
+package com.tk.d8;
+
+public class Account {
+  private int accno;
+  private String name;
+  private double bal;
+  private Bank bank;
+  
+  public Account(int accno, String name,double bal,Bank bank)
+  {
+	  this.accno=accno;
+	  this.name=name;
+	  this.bal=bal;
+	  this.bank=bank;
+  }
+  public int getaccno()
+  {
+	  return accno;
+  }
+  public String getname()
+  {
+	  return name;
+  }
+  public double getbal()
+  {
+	  return bal;
+  }
+  public Bank getbank()
+  {
+	  return bank;
+  }
+  
+  public void deposit(double amount)
+  {
+	  bank.deposit(this,amount);
+  }
+ public void withraw(double amount)
+ {
+	 bank.withraw(this,amount);
+ }
+  
+  
+  
+  public void setaccno(int accno)
+  {
+	  this.accno=accno;
+  }
+  public void setname(String name)
+  {
+	  this.name=name;
+  }
+  public void setbal(double bal)
+  {
+	  this.bal=bal;
+  }
+  public void setbank(Bank bank)
+  {
+	  this.bank=bank;
+  }
+  @Override
+  public  String toString()
+  {
+	  return "Account [accno="+accno+",name ="+name+",bal="+bal+"]";
+  }
+	  
+  
+}
---
+++ tns/src/com/tk/d8/Bankimp.java
@@ -0,0 +1,30 @@
+package com.tk.d8;
+
+public class Bankimp implements Bank {
+  
+	@Override
+	public void deposit(Account account,double amount)
+	{
+		if(amount>deposit_limit)
+		{
+			System.out.println("deposit not possible");
+		}
+		else
+		{
+			account.setbal(account.getbal()+amount);
+			System.out.println("deposite "+amount);
+		}
+		
+	}
+	@Override
+	public void withraw(Account account,double amount)
+	{
+		if(account.getbal()-amount>=min_bal)
+		{
+			account.setbal(account.getbal()-amount);
+			System.out.println("withraw"+amount);
+		}
+		
+	}
+   	 
+}
