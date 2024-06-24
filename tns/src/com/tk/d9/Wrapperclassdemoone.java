20cceb0c7ce191946853c1221bdd3a5407401779
Taheer Khan <Taheer Khan@DESKTOP-CLASFLV>
2024-06-24 19:53:22 +0530

    demo

---
+++ tns/src/com/tk/d9/Wrapperclassdemoone.java
@@ -0,0 +1,16 @@
+package com.tk.d9;
+
+public class Wrapperclassdemoone {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		int a=Integer.parseInt("7"); //method for integer which part of integer class-parseint
+		int b=Integer.parseInt("9");  //parse means converting string into integer
+		int c=a+b;
+		System.out.println(c);
+		
+		
+
+	}
+
+}
